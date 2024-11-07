265978f23e1b976505b40200a0eead228c430b9e
Liberly T. Ch. Djara <2381010@unai.edu>
2024-11-07 21:48:42 +0700

    Handson 8

---
+++ services/TodoListServiceImpl.java
@@ -0,0 +1,41 @@
+package services;
+
+import entities.TodoList;
+import repositories.TodoListRepository;
+
+public class TodoListServiceImpl implements TodoListService {
+    private final TodoListRepository todoListRepository;
+
+    public TodoListServiceImpl(final TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+
+    @Override
+    public TodoList[] getTodoList() {
+        return todoListRepository.getAll();
+    }
+
+    @Override
+    public Boolean removeTodoList(Integer number) {
+        return todoListRepository.remove(number);
+    }
+
+    @Override
+    public void addTodoList(String todo) {
+        if (todo.isEmpty() || todo.isBlank()) {
+            System.out.println("Masukkan todo dengan benar");
+            return;
+        }
+        TodoList todoList = new TodoList();
+        todoList.setTodo(todo);
+        todoListRepository.add(todoList);
+    }
+
+    @Override
+    public Boolean editTodoList(Integer number, String todo) {
+        TodoList todoList = new TodoList();
+        todoList.setId(number);
+        todoList.setTodo(todo);
+        return todoListRepository.edit(todoList);
+    }
+}
